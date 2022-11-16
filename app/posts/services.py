e90fe6622f1c8d50de791d068e17db4576ac4ea8
Ananev-Alexandr <ananevalexandr@mail.ru>
2022-11-16 15:23:36 +0300

    #NT create Post

--- app/posts/services.py
+++ app/posts/services.py
@@ -1,5 +1,24 @@
-# # работа с бд
-# class UserService():
-#     @classmethod
-#     def creat_user():
-#         pass
+from data_base.models import Post, db
+
+
+class PostService():
+    @classmethod
+    def creat_post(cls, params: dict) -> dict:
+        text_post = params.get('text_post')
+        if PostService.validate_post(text_post) is False:
+            return False
+        post = Post()
+        post.text_post = text_post
+        #TODO user_id, MOCK
+        post.user_id = 42
+        db.session.add(post)
+        db.session.commit()
+        return {'message': 'Success add post!'}
+
+    @classmethod 
+    def validate_post(cls, text_post):
+        if text_post is None:
+            print("Пост не может быть пустым!")
+            return False
+        return True
+        
